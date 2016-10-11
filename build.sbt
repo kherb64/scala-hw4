a3409b70ebaf5cf6bf150f2b79375a714dcead0f
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2016-10-11 16:33:54 +0200

    Hurzi class
    do not play with value classes
    add gitignore
    update build.sbt

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "hw4"
 
-version := "1.0"
+version := "1.0.0"
 
 scalaVersion := "2.11.8"
 
---
+++ src/main/scala/Hurzi.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by herb on 11.10.2016.
+  * Conclusion: do not use user definde value classes at an early stage of knowledge!
+  */
+
+class Hurzi /* extends Int */ {
+  def unary_~ = { -1 }
+  def == = { true }
+  def toDouble = { 1.0d }
+  def == (b:Byte) = { true }
+}
