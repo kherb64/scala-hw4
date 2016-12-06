f6d666f47ed134414625bec30b9191a73374e6ac
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2016-12-06 09:25:08 +0100

    Preparation for Sebs phone number

---
+++ src/main/scala/DecrypterApp.scala
@@ -0,0 +1,35 @@
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+object DecryptApp extends App {
+
+  object Decrypter {
+
+    private val decoder = Base64.getDecoder
+
+    def decrypt(text: String, key: String): String = {
+      val encryptedDecoded = decoder.decode(text)
+      new String(cipher(key).doFinal(encryptedDecoded))
+    }
+
+    private def cipher(key: String) = {
+      val cipher = Cipher.getInstance("AES")
+      val aesKey = new SecretKeySpec(key.getBytes, "AES")
+      cipher.init(Cipher.DECRYPT_MODE, aesKey)
+      cipher
+    }
+
+  }
+
+  val encrypted = "qcbswidBdostEhxsUOh+8EWMDDRtBC+bvLXg9jzoHww="
+
+  // Replace FFFFFF and LLLLLL by the first-name and last-name of the
+  // creator of Scala, respectively. First letters uppercased, rest
+  // lowercased (That is "Firstname___Lastname").
+  // Keep the 3 underscores in the middle.
+  // The resulting String should be 16 letters long.
+  val key = "Martin___Odersky"
+
+  println("Sebastian's mobile phone number is: " + Decrypter.decrypt(encrypted, key))
+}
