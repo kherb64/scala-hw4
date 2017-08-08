453af75f3b4a76161aa7b82c2fe1c916d87631ba
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2017-08-08 15:46:08 +0200

    Pizza added
    various override examples

---
+++ src/main/scala/hw4/traits/Pizza.scala
@@ -0,0 +1,13 @@
+package hw4.traits
+
+trait PizzaTrait {
+  var numToppings: Int // abstract
+  var size = 14 // concrete
+  val maxNumToppings = 10 // concrete
+}
+
+class Pizza extends PizzaTrait {
+  var numToppings = 0 // override not needed because of var
+  size = 16 // 'var' and 'override' not needed
+  override val maxNumToppings = 11 // override and val needed because of val
+}
