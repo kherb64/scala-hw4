4bb1cd9d98997be1ae7cc94cd257b272cd2c346d
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2017-08-09 17:23:16 +0200

    new test classes

--- src/test/scala/hw4/classes/PizzaSpec.scala
+++ src/test/scala/hw4/classes/PizzaSpec.scala
@@ -1,44 +1,54 @@
 package hw4.classes
 
-import hw4.UnitSpec
+import hw4.UnitSpec2
 import hw4.classes.Pizza._
 
-class PizzaSpec extends UnitSpec {
+class PizzaSpec extends UnitSpec2 {
 
-  "Tests" should "be working" in {
+  "Tests should be working" in {
     true shouldBe true
   }
 
-  val p = new Pizza()
-  s"Pizza()" should "not be null" in {
-    p should not be null
+  "Pizza() should" - {
+    val p = new Pizza()
+    val newSize = 15
+    val newCrust = "THICK"
+
+    "not be null" in {
+      p should not be null
+    }
+
+    "be an instance of Pizza" in {
+      p.isInstanceOf[Pizza] shouldBe true
+    }
+
+    s"have a default size of $DEFAULT_CRUST_SIZE" in {
+      p.crustSize shouldBe DEFAULT_CRUST_SIZE
+    }
+    s"have a default type of $DEFAULT_CRUST_TYPE" in {
+      p.crustType shouldBe DEFAULT_CRUST_TYPE
+    }
+
+    s"read A $DEFAULT_CRUST_SIZE inch pizza with a $DEFAULT_CRUST_TYPE crust" in {
+      p.toString shouldBe s"A $DEFAULT_CRUST_SIZE inch pizza with a $DEFAULT_CRUST_TYPE crust"
+    }
+
+    s"have a new size of $newSize" in {
+      p.crustSize = newSize
+      p.crustSize shouldBe newSize
+    }
+
+    s"have a new $newCrust crust" in {
+      p.crustType = newCrust
+      p.crustType shouldBe newCrust
+    }
+
+    s"read A $newSize inch pizza with a $newCrust crust" in {
+      p.toString shouldBe s"A $newSize inch pizza with a $newCrust crust"
+    }
+
   }
 
-  "it" should "be an instance of Pizza" in {
-    p.isInstanceOf[Pizza] shouldBe true
-  }
 
-  "it" should s"have a size of $DEFAULT_CRUST_SIZE" in {
-    p.crustSize shouldBe DEFAULT_CRUST_SIZE
-  }
-  "it" should s"have a type of $DEFAULT_CRUST_TYPE" in {
-    p.crustType shouldBe DEFAULT_CRUST_TYPE
-  }
-
-  "it" should s"read A $DEFAULT_CRUST_SIZE inch pizza with a $DEFAULT_CRUST_TYPE crust" in {
-    p.toString shouldBe s"A $DEFAULT_CRUST_SIZE inch pizza with a $DEFAULT_CRUST_TYPE crust"
-  }
-
-  val newSize = 14
-  "it" should s"have a new size of $newSize" in {
-    p.crustSize = newSize
-    p.crustSize shouldBe newSize
-  }
-
-  val newCrust = "THICK"
-  "it" should s"read A $newSize inch pizza with a $newCrust crust" in {
-    p.crustType = newCrust
-    p.toString shouldBe s"A $newSize inch pizza with a $newCrust crust"
-  }
 
 }
