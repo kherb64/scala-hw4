d19a8bbfde281acd95d80b14d11659354a2c9f5d
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2017-08-30 12:33:38 +0200

    reading file
    loan pattern

--- src/main/scala/at/co/sdt/herb/hw4/files_and_processes/Read.scala
+++ src/main/scala/at/co/sdt/herb/hw4/files_and_processes/Read.scala
@@ -1,6 +1,6 @@
 package at.co.sdt.herb.hw4.files_and_processes
 
-import java.io.{ File => JFile }
+import java.io.{ FileNotFoundException, IOException, File => JFile }
 
 import at.co.sdt.herb.hw4
 
@@ -8,7 +8,8 @@ import scala.io.Source
 
 object Read extends App {
   // new Read().readConcise()
-  new Read().readProperly()
+  //new Read().readProperly()
+  new Read().readWithLoanPattern()
 }
 
 class Read {
@@ -37,12 +38,57 @@ class Read {
   }
 
   def readProperly(): Unit = {
-    println("reading porperly")
+    println("reading properly")
     val bufferedSource = Source.fromFile(resourcesFile("textfile.txt"))
     for (line <- bufferedSource.getLines) {
       println(line)
     }
     bufferedSource.close
-    println("read porperly")
+    println("read properly")
   }
+
+  /**
+    * reads the file with the 'loan pattern'
+    */
+  def readWithLoanPattern(): Unit = {
+    println("try reading")
+    try {
+      val bufferedSource = Source.fromFile(resourcesFile("textfile.txt"))
+
+      try {
+        for (line <- bufferedSource.getLines()) {
+          println(line)
+        }
+      } catch {
+        case e: IOException => println(e)
+      } finally {
+        bufferedSource.close
+      }
+
+      println("read successful")
+    } catch {
+      case e: FileNotFoundException => println(e)
+    }
+  }
+
+  def readTextFile(filename: String): Option[List[String]] = {
+    import Control._
+    try {
+      val lines = using(io.Source.fromFile(filename)) { source =>
+        (for (line <- source.getLines) yield line).toList
+      }
+      Some(lines)
+    } catch {
+      case _: Exception => None
+    }
+  }
+}
+
+object Control {
+  def using[A <: { def close(): Unit }, B](resource: A)(f: A => B): B =
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
 }
