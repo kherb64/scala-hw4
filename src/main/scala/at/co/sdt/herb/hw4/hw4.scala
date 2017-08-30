83dd5bffdaf19e1a6185bdb44b6ac590d10af02f
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2017-08-30 10:44:34 +0200

    improved names
    for project and resources directory

--- src/main/scala/at/co/sdt/herb/hw4/hw4.scala
+++ src/main/scala/at/co/sdt/herb/hw4/hw4.scala
@@ -17,8 +17,8 @@ package object hw4 {
 
   import java.io.{ File => JFile }
 
-  lazy val projDir: JFile = new JFile("C:/IdeaProjects/scala-hw4/").getCanonicalFile
-  lazy val resDir: JFile = new JFile(projDir + "/src/main/resources/").getCanonicalFile
+  lazy val projectDir: JFile = new JFile("C:/IdeaProjects/scala-hw4/").getCanonicalFile
+  lazy val resourcesDir: JFile = new JFile(projectDir + "/src/main/resources/").getCanonicalFile
 
 }
 
