0d57ae26a01c886d79addd8c3d97b8b500b2c36b
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2017-08-31 13:53:27 +0200

    opening with Try

--- src/main/scala/at/co/sdt/herb/hw4/files_and_processes/TextFile.scala
+++ src/main/scala/at/co/sdt/herb/hw4/files_and_processes/TextFile.scala
@@ -1,47 +1,70 @@
 package at.co.sdt.herb.hw4.files_and_processes
 
-import scala.io.BufferedSource
+import java.io.{ File => JFile }
 
-object TextFile extends App {
+import scala.io.BufferedSource
+import scala.util.{ Failure, Success, Try }
+
+object TextFileDemo extends App {
 
   import at.co.sdt.herb.hw4
 
-  val srcFile = hw4.resourcesDir + "/textfile.txt?"
-  val lineCount = new TextFile(srcFile).lineCount
-  println(s"$srcFile has $lineCount lines")
-  val lineCount2 = new TextFile(srcFile).lineCount2
-  println(s"$srcFile has $lineCount2 lines")
-  val lineCount3 = new TextFile(srcFile).lineCount3
-  println(s"$srcFile has $lineCount3 lines")
+  val fileName = hw4.resourcesDir + "/textfile.txt?"
+  val textFileOpt = TextFile.ofName(fileName)
+  textFileOpt match {
+    case Some(tf) => println(s"$fileName has ${ tf.lineCount } ${ tf.lineCount2 } ${ tf.lineCount3 } lines")
+    case None => println(s"File $fileName not found")
+  }
+
 }
 
-class TextFile(fileName: String) {
+object TextFile {
+  def apply(file: JFile): TextFile = {
+    new TextFile(file)
+  }
 
-  import java.io.{ File => JFile }
+  def ofName(fileName: String): Option[TextFile] = {
+    // open file by name safely with Try
+    val sourceFile: Try[JFile] = Try(new JFile(fileName).getCanonicalFile)
+    sourceFile match {
+      case Success(tf) => Some(new TextFile(tf))
+      case Failure(ex) =>
+        println(ex)
+        // ex.printStackTrace()
+        None
+    }
+  }
+}
 
-  // TODO: how to open files safely? with Option or so?
-  val sourceFile: JFile = new JFile(fileName).getCanonicalFile
-  val source: BufferedSource = io.Source.fromFile(sourceFile)
+class TextFile(file: JFile) {
+
+  def source: BufferedSource = io.Source.fromFile(file)
 
   // for {line <- source.getLines()} { println(line) }
 
   def lineCount: Long = {
-    val NewLine = 10
+    val NewLine = '\n'
     var newlineCount = 0L
     for {
       char <- source
       if char.toByte == NewLine
     } newlineCount += 1
+    source.close()
     newlineCount + 1
   }
 
   def lineCount2: Long = {
     var newlineCount = 0L
-    for {line <- source.getLines()} {
+    for {_ <- source.getLines()} {
       newlineCount += 1
     }
+    source.close()
     newlineCount
   }
 
-  def lineCount3: Long = source.getLines().length
+  def lineCount3: Long = {
+    val newlineCount = source.getLines().size
+    source.close()
+    newlineCount
+  }
 }
--- src/main/scala/at/co/sdt/herb/hw4/hw4.scala
+++ src/main/scala/at/co/sdt/herb/hw4/hw4.scala
@@ -1,5 +1,7 @@
 package at.co.sdt.herb
 
+import scala.util.{ Failure, Success, Try }
+
 /**
   * my first package object
   */
@@ -20,5 +22,22 @@ package object hw4 {
   private lazy val userDir = System.getProperty("user.dir")
   lazy val projectDir: JFile = new JFile(userDir + "/").getCanonicalFile
   lazy val resourcesDir: JFile = new JFile(projectDir + "/src/main/resources/").getCanonicalFile
+
+  trait File {
+    def ofName(fileName: String): Option[JFile] = {
+      // open file by name safely with Try
+      val file: Try[JFile] = Try(new JFile(fileName).getCanonicalFile)
+      file match {
+        case Success(f) => Some(f)
+        case Failure(ex) =>
+          println(ex)
+          // ex.printStackTrace()
+          None
+      }
+    }
+  }
+
+  object File extends File {
+  }
 
 }
