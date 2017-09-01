d314ab7167031c5bf56b724a831c68d80c5e8ed5
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2017-09-01 17:02:48 +0200

    more commands

--- src/main/scala/at/co/sdt/herb/hw4/external_commands/ExternalCommand.scala
+++ src/main/scala/at/co/sdt/herb/hw4/external_commands/ExternalCommand.scala
@@ -1,15 +1,28 @@
 package at.co.sdt.herb.hw4.external_commands
 
+import scala.util.{ Success, Try }
 import sys.process._
 
 class ExternalCommand {
-  def cmd(cmdName: String = ""): Int = {
-    s"cmd.exe /c $cmdName".!
+  def cmd(cmd: String = ""): Int = {
+    Try(cmd.!) match {
+      case Success(i) => i
+      case _ => -1
+    }
+  }
+
+  def shellCmd(cmdName: String = ""): Int = {
+    cmd(s"cmd.exe /c $cmdName")
   }
 }
 
 object ExternalCommandDemo extends App {
   val ext = new ExternalCommand
-  ext.cmd("dir")
-  ext.cmd("dir \\temp")
+  //ext.shellCmd(" dir ")
+  //println(ext.shellCmd("dir \\temp"))
+  ext.cmd("java -version")
+  println(ext.cmd("sbt -version"))
+  //val exitCode = Process("dir").!
+  val p = Process("cmd /c dir")
+  p.!
 }
