1d09a60f67d4bde6efba4abeb371878aa41b4f2a
Herbert Kreutzer <herbert.kreutzer@sdt.co.at>
2018-01-08 11:21:27 +0100

    xml file read
      refined node display

--- src/main/scala/at/co/sdt/herb/hw4/objects/myClassOf.scala
+++ src/main/scala/at/co/sdt/herb/hw4/objects/myClassOf.scala
@@ -6,10 +6,19 @@ object myClassOf extends App {
   // println(s"${this.getClass} started at $executionStart")
 
   val hello = <p>Hello,
+    world</p>
+  println(hello.getClass)
+
+  val hello2 = <p>Hello,
     <br/>
     world</p>
   println(hello.getClass)
 
-  hello.child.foreach(e => println(e.getClass))
-  println(s"${ this.getClass } took ${ currentTime - executionStart } ms")
+  val sb = new StringBuilder
+  hello.child.foreach(e => println(s"${ e.getClass } ${ e.text }"))
+  hello2.child.foreach(e => {
+    sb.clear
+    println(s"${ e.getClass } ${ e.nameToString(sb) }=${ e.text }")
+  })
+  println(s"${ this.getClass.getSimpleName } took ${ currentTime - executionStart } ms")
 }
